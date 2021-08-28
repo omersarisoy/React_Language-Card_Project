cc0fd3492bddc4d5126f5c0bdfe0c2063510b903
omersarisoy <omersarisoy1993@gmail.com>
2021-08-28 12:17:41 +0300

    updated

--- src/components/card.js
+++ src/components/card.js
@@ -1,8 +1,11 @@
 import logo from "../assets/react.svg";
 import { categories } from "../helper/data";
 import "./card.css";
+import { useState } from "react";
+
 
 const Card = () => {
+  const [showImage, setShowImage] = useState("false")
   return (
     <>
     <div className="container">
@@ -17,20 +20,24 @@ const Card = () => {
         <div className="all-cards">
            
           {categories.map((item) => (
-            <div className="card">
-                <div className="image">
-                  <img src={item.img} />
+            <div className="card" onClick={ ()=>setShowImage(!showImage)}>
+              {showImage ? (<><div className="image">
+                <img src={item.img} />
                 </div>
-  
+              
                 <div className="name">
                   <h4> {item.name} </h4>
-                </div>
-  
-                <div className="options">
+                </div></>)
+                :
+                (<div className="opt">
                   <li> {item.options[0]} </li> 
                   <li> {item.options[1]} </li>
                   <li> {item.options[2]} </li>
-                </div>
+                </div>)}
+
+                
+  
+                
             </div>
             
             ))}
